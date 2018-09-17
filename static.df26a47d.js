107ce5b504d60d1cecb5bef61547a5c98c0251b5
Nina Olofsson <nina.olofsson@tink.se>
2018-09-17 20:35:49 +0200

    Updates

--- static.df26a47d.js
+++ static.df26a47d.js
@@ -67,7 +67,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/";
+/******/ 	__webpack_require__.p = "https://ninaolo.github.io/react-static-github-pages-example/";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 10);
@@ -1528,4 +1528,4 @@ function toComment(sourceMap) {
 /***/ })
 /******/ ]);
 });
-//# sourceMappingURL=static.800e7dd7.js.map
+//# sourceMappingURL=static.df26a47d.js.map
